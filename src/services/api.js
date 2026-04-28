187ebb395dd757e14474f637e8125583f905b6f9
Siddhant Sonawane <siddhantsonawane2004@gmail.com>
2026-04-28 18:15:47 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://supplychain-xkjw.vercel.app/api/',
+  baseURL: 'https://supplychain-p614.onrender.com/api/',
 });
 
 export default api;
